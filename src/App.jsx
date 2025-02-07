e2106f7b886d4b9b5e98234d3cd3ea729dab56e6
llanesleonardo <llanesleonardo@gmail.com>
2025-02-06 17:51:47 -0700

    404

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from "./Layout/Layout.jsx"
 import HomePage from "./Views/HomePage.jsx"
 import BusinessCard from "./Views/BusinessCard.jsx"
+import NotFound from "./Views/NotFound.jsx";
 
 function App() {
 //<Route exact path="/business-card" element={<BusinessCard />} />
@@ -11,7 +12,9 @@ function App() {
           <Router>
         <Layout>
           <Routes>
-            <Route exact path="/" element={<HomePage />} />
+            {/* <Route exact path="/" element={<HomePage />} /> */}
+                    {/* Catch-all route for 404 */}
+          <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </Router>
@@ -20,3 +23,25 @@ function App() {
 }
 
 export default App
+
+
+/**
+ * {
+    "routes": [
+      {
+        "route": "/*",
+        "statusCode": 200
+      }
+    ],
+    "navigationFallback": {
+        "rewrite": "index.html",
+        "exclude": ["/images/*.{png,jpg,gif}", "/css/*"]
+      },
+      "responseOverrides": {
+        "404": {
+          "rewrite": "/404.html"
+        }
+      }
+  }
+ * 
+ */
---
+++ src/Views/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div style={{display:'flex', flexDirection:'row',justifyContent:'center',alignItems:'center',minHeight:'100vh'}}>
+      <div>404 - Page not Found</div>
+    </div>
+  )
+}
